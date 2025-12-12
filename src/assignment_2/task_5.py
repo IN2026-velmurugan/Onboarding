592ee59f973fcf6462fdcaad1ddd69a775725008
IN2026-velmurugan <velmurugan.kl@solitontech.com>
2025-12-12 09:46:45 +0530

    ci: checking CI checks

--- src/assignment_2/task_5.py
+++ src/assignment_2/task_5.py
@@ -11,6 +11,6 @@ print("Before function call, number_1 =", number_1)
 function()
 print("After function call, number_1 =", number_1)
 try:
-    print("Trying to access number_2 outside function:", number_2)
+    print("Trying to access number_2 outside function", number_2)
 except Exception as e:
     print("Error because the local scope cannot be accessed outside scope:", e)
