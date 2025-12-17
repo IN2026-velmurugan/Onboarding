423b93a02b875045df578ab934d8bdf86c4fa893
IN2026-velmurugan <velmurugan.kl@solitontech.com>
2025-12-17 17:38:36 +0530

    refactor: resolved the pr comments to make the pipeline work

--- src/assignment_2/task_4.py
+++ src/assignment_2/task_4.py
@@ -2,13 +2,13 @@
 
 string_1 = "Hello, this is a test string for checking immutability."
 
-# try:
-#     string_1[0] = "h"
-# except Exception as e:
-#     print("causes Error because string is immutable:", e)
+try:
+    string_1[0] = "h"  # type: ignore
+except Exception as e:
+    print("causes Error because string is immutable:", e)
 
-# integer_1 = 1000
-# try:
-#     integer_1[0] = 1
-# except Exception as e:
-#     print("causes Error because integer is immutable:", e)
+integer_1 = 1000
+try:
+    integer_1[0] = 1  # type: ignore
+except Exception as e:
+    print("causes Error because integer is immutable:", e)
