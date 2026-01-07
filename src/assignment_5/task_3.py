c24e3e5b0f4078f5f2f15bdf7c833297327bdd6c
IN2026-velmurugan <velmurugan.kl@solitontech.com>
2026-01-07 09:45:07 +0530

    feat: implement tasks from assignment 5 (#5)
    * feat: implemented modules based on data types
    
    * fix: resolved the PR comments
    
    * fix: test
    
    * fix: test
    
    * fix: modified the configurations to support Black
    
    * docs: updated the module documentation
    
    * fix: pipeline error
    
    * fix: pipeline error
    
    * fix: pipeline error fix
    
    * fix: pipeline not running
    
    * fix: pipeline not running
    
    * fix: pipeline not running
    
    * fix: pipeline not running
    
    * fix: pipeline not running
    
    * fix: pipeline not running
    
    * fix: fixing pipeline run error
    
    * feat: implemented methods to understand the variables
    
    * feat: added NI-python-styleguide
    
    * ci: checking CI checks
    
    * ci: checking CI checks
    
    * ci: checking CI checks
    
    * feat: started with expression evaluation
    
    * feat: completed the expression evaluator
    
    * feat: completed the expression evaluator
    
    * feat: implemented task 2
    
    * feat: implemented task 2 to find early, latest and unique dates
    
    * feat: started with task 3
    
    * feat: Implemented task 3 to count the files
    
    * feat: Implemented task 3 to count the files using pathlib
    
    * other: resolving pr1
    
    * fix: Resolved the PR comments including the assignment_1 for NPS
    
    * feat: implemented task 4 for flattening JSON
    
    * feat: implementd task 5 for mock database
    
    * fix: removed black from pipeline
    
    * other: testing the format
    
    * other: testing the format
    
    * refactor: fixed the documentations according to does form
    
    * other: saving the changes
    
    * feat:implemented task 1 for extarcting the square of primes
    
    * feat: implemented the task 2
    
    * feat: implemented task 3 to map the values
    
    * fix: fixed pipeline error on package imports
    
    * refactor: resolved pr comments on logical errors and docstring
    
    * chore: resolved the PR comments on docstring and types definition
    
    * refactor: resolved PR comments on docstring and logics
    
    Made the docstring do form and explored on os.path and implemented the changes
    
    * refactor: Resolve PR comments on docstring
    
    Updated the Docstring to do form and made the changes in the function to make it more robust
    
    * fix: Update docstring and minor logic refactor
    
    Handled exceptions gracefully and covering most of found corner cases
    
    * refactor: update function names and gracefull error handling
    
    Handled exception gracefully for the scripts that require user inputs
    
    * chore: fix in mypy type hinting
    
    * refactor: main refactor
    
    Making the code testable and allowing the user to retry
    
    * refactor: change function name

---
+++ src/assignment_5/task_3.py
@@ -0,0 +1,39 @@
+"""Function for numpy array value mapping."""
+
+import math
+
+import numpy as np
+from sklearn.datasets import load_iris  # type: ignore
+
+
+def get_dictionary_mapping(
+    data: np.ndarray, feature_names: list[str]
+) -> list[dict[str, np.float64]]:
+    """Map the normalized value to the features.
+
+    Args:
+        data: 2D numpy array data.
+        feature_names: Feature names for the numpy array.
+
+    Returns:
+        The normalized value mapped to the feature.
+    """
+    means = {i: sum(data[:, i]) / len(data) for i in range(data.shape[1])}
+
+    std_deviations = {
+        i: math.sqrt((sum([(x - means.get(i)) ** 2 for x in data[:, i]])) / (len(data) - 1))
+        for i in range(data.shape[1])
+    }
+
+    dict_map = [
+        {feature_names[i]: ((item[i] - means[i]) / std_deviations[i]) for i in range(data.shape[1])}
+        for item in data
+    ]
+    return dict_map
+
+
+if __name__ == "__main__":
+    iris = load_iris()
+    data = iris.data  # type: ignore
+    feature_names = iris.feature_names  # type: ignore
+    print(get_dictionary_mapping(data, feature_names))
